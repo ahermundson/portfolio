96cb1ff9577f4728813f397b415e60c28643e7d1
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2017-02-06 21:24:30 -0600

    adjusted styling of skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,26 +4,28 @@ export const Skills = () => (
 
   <div id="skills-container">
     <h2 id="skills-header">Technical Skills</h2>
-    <div id="skills-column-one">
-      <ul>
-        <li>MEAN Stack (MongoDB, express.js, AngularJS, Node.js)</li>
-        <li>Mongoose</li>
-        <li>Firebase Authentication</li>
-        <li>Javascript</li>
-        <li>HTML5</li>
+    <div id="skills-list">
+      <div id="skills-column-one">
+        <ul>
+          <li>MEAN Stack (MongoDB, express.js, AngularJS, Node.js)</li>
+          <li>Mongoose</li>
+          <li>Firebase Authentication</li>
+          <li>Javascript</li>
+          <li>HTML5</li>
 
-      </ul>
-    </div>
-    <div id="skills-column-two">
-      <ul>
-        <li>APIs</li>
-        <li>Heroku</li>
-        <li>AJAX</li>
-        <li>CSS</li>
-        <li>PostgresQL</li>
-        <li>Git</li>
-        <li>Bootstrap</li>
-      </ul>
+        </ul>
+      </div>
+      <div id="skills-column-two">
+        <ul>
+          <li>APIs</li>
+          <li>Heroku</li>
+          <li>AJAX</li>
+          <li>CSS</li>
+          <li>PostgresQL</li>
+          <li>Git</li>
+          <li>Bootstrap</li>
+        </ul>
+      </div>
     </div>
   </div>
 );
