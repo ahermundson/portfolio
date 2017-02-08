24c6a77e76654d6306a6ad388f675ff045a0e7b3
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2017-02-08 16:00:35 -0600

    added project images and descripriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,11 +2,32 @@ import '../stylesheets/main.scss'
 
 export const Projects = () => (
   <div id="project-container">
-    <div className="project">
-      <h4>A Book A Week</h4>
+    <h2>Projects</h2>
+    <div id="a-book-a-week">
+      <div className="project-image">
+        <img src="/assets/a-book-a-week.png" />
+      </div>
+      <div className="project-info">
+        <h3>A Book A Week</h3>
+        <ul>
+          <li>A Book A Week is an app that allows you to easily track your progress towards reading a book a week for a year.</li>
+          <li>A Book A Week helps you stay on track to read a book a week for a year by sending you daily emails and giving you specifics on how many pages you need to read per day to stay on track to meet your goal. </li>
+          <li>Technologies Used: AngularJS, MongoDB, Mongoose, Nodemailer, Google Books API, Node.js, express and Firebase Authentication.</li>
+        </ul>
+      </div>
     </div>
-    <div className="project">
-      <h4>American Drapery Systems</h4>
+    <div id="american-drapery-systems">
+      <div className="ads-project-info">
+        <h3>A Book A Week</h3>
+        <ul>
+          <li>A Book A Week is an app that allows you to easily track your progress towards reading a book a week for a year.</li>
+          <li>A Book A Week helps you stay on track to read a book a week for a year by sending you daily emails and giving you specifics on how many pages you need to read per day to stay on track to meet your goal. </li>
+          <li>Technologies Used: AngularJS, MongoDB, Mongoose, Nodemailer, Google Books API, Node.js, express and Firebase Authentication.</li>
+        </ul>
+      </div>
+      <div className="ads-project-image">
+        <img src="/assets/a-book-a-week.png" />
+      </div>
     </div>
   </div>
 )
