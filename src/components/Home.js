7277445f2c6142e290a3c1bd8d9271ee5f15e9b4
Alex Hermundson <alex.hermundson@gmail.com>
2017-02-10 10:54:56 -0600

    fixed background image cover bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,5 +4,11 @@ export const Home = () => (
   <div id="home-container">
     <h1>Hi, I'm Alex Hermundson.</h1>
     <h4>I'm a full-stack developer in Minneapolis, MN and am currently seeking employment.</h4>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
   </div>
 )
