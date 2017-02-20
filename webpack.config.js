86f7eb629494675c48a2ef968d4b98988ec84969
Alex Hermundson <alex.hermundson@gmail.com>
2017-02-20 16:35:17 -0600

    testing removing / from publich path output

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/public/'
+        publicPath: '/public'
   },
   devServer: {
     inline: true,
