5c5d8248433e52bfeb405be8c32927891dd00a85
Alex Hermundson <alex.hermundson@gmail.com>
2017-02-20 16:36:49 -0600

    removed first output of bundle and added / back to end of public

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,10 @@ var path = require('path')
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    path: "/dist/assets",
-    filename: "bundle.js",
-    publicPath: "assets"
-  },
-  entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/public'
+        publicPath: '/public/'
   },
   devServer: {
     inline: true,
