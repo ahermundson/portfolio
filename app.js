0d2166d2ff2dbe2b14adde0e9534278df1558073
Alex Hermundson <alex.hermundson@gmail.com>
2017-02-20 16:09:06 -0600

    adding app.js for server details

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var portDecision = process.env.PORT || 3000;
+
+app.use(express.static('server/public'));
+
+//Static Files
+app.get('/', function(req, res){
+  res.sendFile(path.resolve('dist/index.html'));
+});
+
+app.listen(portDecision, function(){
+  console.log('running on port', portDecision);
+});
